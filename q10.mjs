787477417b55992e557ae980c06bc6a10fc66e12
shoaib memon <shoaibattari83@gmail.com>
2022-10-18 15:10:59 +0500

    3rd commit

---
+++ q10.mjs
@@ -0,0 +1,18 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file.
+// Then write one sentence describing what the program does.
+
+// shoaib memon
+// this program your number add two return answer
+
+
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+console.log(`your number is +2`);
+
+
+let number = +prompt("write number ");
+console.log(number + 2);
+
---
+++ q9.mjs
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable,
+// create a message that reveals your favorite number. Print that message.
+
+let favorite_number =  26;
+let result = (`your favourite number is ${favorite_number}`);
+console.log(result);

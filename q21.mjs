8d61cd5c20dedd03d4cc367760329950faa970e5
shoaib memon <shoaibattari83@gmail.com>
2022-10-28 01:15:42 +0500

    add

---
+++ q21.mjs
@@ -0,0 +1,8 @@
+// They think of something you could store in a JavaScript Object. 
+// Write a program that creates Objects containing these items.
+
+let StudentName = {
+    S_name: "shoiab",
+    F_name: "sattar",
+    Age: "30",
+};
